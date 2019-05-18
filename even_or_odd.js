6a04041b8fab0212055bce8eb030f43e3cbd7458
Ziaur Abdirahman <adtechzia@gmail.com>
2019-05-19 00:26:22 +0600

    Odd or even using only if statement

---
+++ even_or_odd.js
@@ -0,0 +1,11 @@
+// Using Only If Syntax -- ODD OR EVEN
+
+var a = 5
+
+if ((a % 2) === 0) {
+    console.log(a + ' is an even number')
+}
+
+if ((a % 2) !== 0) {
+    console.log(a + ' is an odd number ')
+}
